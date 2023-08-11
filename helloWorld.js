af69522501a43d176cab4785673f8e4a3d615d36
alistoicakovacs <alistoicakovacs@icloud.com>
2023-08-11 23:22:16 +0200

    test

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,17 @@
 Script started on Sun May 14 21:04:09 2023
+[1m[7m%[27m[1m[0m                                                                                                                                               [0m[27m[24m[Jalistoicakovacs@Alis-Air javascript-exercises % [K[?2004hhhellowor    W o   h  rrun  r  ggit push    ad   ad d d.  .[?2004l
+[1m[7m%[27m[1m[0m                                                                                                                                               [0m[27m[24m[Jalistoicakovacs@Alis-Air javascript-exercises % [K[?2004hi ggit commit-m   -  - m    t -m "test"[?2004l
+[main a79fbb9] test
+ 2 files changed, 2 insertions(+), 1 deletion(-)
+ create mode 100644 helloWorld.js
+[1m[7m%[27m[1m[0m                                                                                                                                               [0m[27m[24m[Jalistoicakovacs@Alis-Air javascript-exercises % [K[?2004hggit push[?2004l
+Enumerating objects: 8, done.
+Counting objects:  12% (1/8)Counting objects:  25% (2/8)Counting objects:  37% (3/8)Counting objects:  50% (4/8)Counting objects:  62% (5/8)Counting objects:  75% (6/8)Counting objects:  87% (7/8)Counting objects: 100% (8/8)Counting objects: 100% (8/8), done.
+Delta compression using up to 2 threads
+Compressing objects:  25% (1/4)Compressing objects:  50% (2/4)Compressing objects:  75% (3/4)Compressing objects: 100% (4/4)Compressing objects: 100% (4/4), done.
+Writing objects:  20% (1/5)Writing objects:  40% (2/5)Writing objects:  60% (3/5)Writing objects:  80% (4/5)Writing objects: 100% (5/5)Writing objects: 100% (5/5), 536 bytes | 536.00 KiB/s, done.
+Total 5 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas:   0% (0/1)[Kremote: Resolving deltas: 100% (1/1)[Kremote: Resolving deltas: 100% (1/1), completed with 1 local object.[K
+To https://github.com/alistoicakovacs/javascript-exercises.git
+   0747078..a79fbb9  main -> main
+[1m[7m%[27m[1m[0m                                                                                                                                               [0m[27m[24m[Jalistoicakovacs@Alis-Air javascript-exercises % [K[?2004h
